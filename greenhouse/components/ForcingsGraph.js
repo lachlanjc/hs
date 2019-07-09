d135af044afafbea12d87d0ceb9df49b190e8f02
Lachlan Campbell <lachlan@hackclub.com>
2019-07-09 03:43:17 -0400

    Reduce graph line thickness

--- greenhouse/components/ForcingsGraph.js
+++ greenhouse/components/ForcingsGraph.js
@@ -14,12 +14,12 @@ const style = {
     },
     axis: { stroke: colors.black, strokeWidth: 2 },
     ticks: {
-      size: tick => 4,
+      size: () => 4,
       stroke: colors.grey,
       strokeWidth: 2
     },
     grid: {
-      stroke: tick => colors.smoke,
+      stroke: () => colors.smoke,
       strokeWidth: 1
     }
   },
@@ -33,72 +33,70 @@ const style = {
 }
 
 const lines = [
-  { id: 'all', name: 'Overall temperature', color: colors.blue, width: 5 },
-  { id: 'gg', name: 'Greenhouse gases', color: colors.red, width: 4 },
-  { id: 'human', name: 'Human', color: colors.orange, width: 3 },
-  { id: 'natural', name: 'Natural', color: colors.green, width: 3 },
+  { id: 'all', name: 'Overall temperature', color: colors.blue, width: 3 },
+  { id: 'gg', name: 'Greenhouse gases', color: colors.red, width: 2 },
+  { id: 'human', name: 'Human', color: colors.orange, width: 2 },
+  { id: 'natural', name: 'Natural', color: colors.green, width: 2 },
   { id: 'aerosol', name: 'Aerosol use', color: palette.cyan[6], width: 1 },
   { id: 'land', name: 'Land use', color: palette.green[6], width: 1 },
-  { id: 'orbit', name: "Earth's orbit", color: palette.indigo[6], width: 1 },
+  { id: 'orbit', name: 'Earth’s orbit', color: palette.indigo[6], width: 1 },
   { id: 'ozone', name: 'Ozone', color: palette.violet[4], width: 1 },
   { id: 'solar', name: 'Solar activity', color: palette.yellow[6], width: 1 },
   { id: 'volcanic', name: 'Volcanoes', color: colors.pink, width: 1 }
 ]
 
 export default () => (
-  <div>
-    <VictoryChart padding={48} style={{ parent: { fontFamily } }}>
-      <VictoryAxis
-        independentAxis
-        tickValues={[1850, 1875, 1900, 1925, 1950, 1975, 2005]}
-        style={style.axis}
-      />
-      <VictoryAxis
-        dependentAxis
-        tickFormat={tick => tick + ' ºF'}
-        style={style.axis}
-      />
-      {map(
-        [take(lines, 4), takeRight(lines, lines.length - 4)],
-        (lineGroup, i) => (
-          <VictoryLegend
-            key={`legend-${i}`}
-            data={lineGroup}
-            labelComponent={<VictoryLabel padding={0} />}
-            colorScale={map(lineGroup, 'color')}
-            padding={{
-              top: 16,
-              bottom: 0
-            }}
-            symbolSpacer={4}
-            style={style.legend}
-            gutter={4}
-            x={[8, 104][i]}
-            y={8}
-          />
-        )
-      )}
-      {map(reverse(lines), line => (
-        <VictoryLine
-          key={`line-${line.id}`}
-          data={data}
-          x="year"
-          y={line.id}
-          style={{
-            data: {
-              stroke: line.color,
-              strokeWidth: line.width
-            }
+  <VictoryChart padding={48} style={{ parent: { fontFamily } }}>
+    <VictoryAxis
+      independentAxis
+      tickValues={[1850, 1875, 1900, 1925, 1950, 1975, 2005]}
+      style={style.axis}
+    />
+    <VictoryAxis
+      dependentAxis
+      tickFormat={tick => tick + ' ºF'}
+      style={style.axis}
+    />
+    {map(
+      [take(lines, 4), takeRight(lines, lines.length - 4)],
+      (lineGroup, i) => (
+        <VictoryLegend
+          key={`legend-${i}`}
+          data={lineGroup}
+          labelComponent={<VictoryLabel padding={0} />}
+          colorScale={map(lineGroup, 'color')}
+          padding={{
+            top: 16,
+            bottom: 0
           }}
-          animate={{
-            duration: 768,
-            onEnter: {
-              duration: 512,
-              after: () => ({ y: 0 })
-            }
-          }}
+          symbolSpacer={4}
+          style={style.legend}
+          gutter={4}
+          x={[8, 104][i]}
+          y={8}
         />
-      ))}
-    </VictoryChart>
-  </div>
+      )
+    )}
+    {map(reverse(lines), line => (
+      <VictoryLine
+        key={`line-${line.id}`}
+        data={data}
+        x="year"
+        y={line.id}
+        style={{
+          data: {
+            stroke: line.color,
+            strokeWidth: line.width
+          }
+        }}
+        animate={{
+          duration: 768,
+          onEnter: {
+            duration: 512,
+            after: () => ({ y: 0 })
+          }
+        }}
+      />
+    ))}
+  </VictoryChart>
 )
